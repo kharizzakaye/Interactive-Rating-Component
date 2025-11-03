6ec4e47211d928a70b08dc055adf7c1caab36c5f
Khariza <kharizzakaye@gmail.com>
2025-11-03 18:17:08 +0800

    Improve code

--- javascript.js
+++ javascript.js
@@ -1,13 +1,14 @@
 var rating = 0;
+const backgroundColor = "hsla(217, 12%, 63%, 20%)";
 
 function setRating(ratingValue)
 {
     rating = ratingValue;
-    document.getElementById("1").style.backgroundColor = "hsla(217, 12%, 63%, 20%)";
-    document.getElementById("2").style.backgroundColor = "hsla(217, 12%, 63%, 20%)";
-    document.getElementById("3").style.backgroundColor = "hsla(217, 12%, 63%, 20%)";
-    document.getElementById("4").style.backgroundColor = "hsla(217, 12%, 63%, 20%)";
-    document.getElementById("5").style.backgroundColor = "hsla(217, 12%, 63%, 20%)";
+    document.getElementById("1").style.backgroundColor = backgroundColor;
+    document.getElementById("2").style.backgroundColor = backgroundColor;
+    document.getElementById("3").style.backgroundColor = backgroundColor;
+    document.getElementById("4").style.backgroundColor = backgroundColor;
+    document.getElementById("5").style.backgroundColor = backgroundColor;
     
     document.getElementById(rating).style.backgroundColor = "hsl(25, 97%, 53%)";
     document.getElementById(rating).style.color = "white";
